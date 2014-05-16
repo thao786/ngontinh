19a8165233c91febe094f8def1b475ab12e088b8
thao ng <roseskindergarten@gmail.com>
2014-05-16 00:49:03 -0400

    gfrd

--- src/framework/session/session.clj
+++
@@ -1,8 +0,0 @@
-(ns framework.session.session
-	(:import 	(java.security.SecureRandom)))
-
-(def jrandom (java.security.SecureRandom.))
-
-(defn newSid [] 
-	(.toString (new BigInteger 130 jrandom) 32))
-
