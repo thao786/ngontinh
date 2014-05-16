6d021a44eac8ee4bf40c6f8307e2d0c60e7945cc
thao ng <roseskindergarten@gmail.com>
2014-05-16 14:16:31 -0400

    chat

--- src/ngontinh/util/homepage.clj
+++ src/ngontinh/util/homepage.clj
@@ -1,23 +1,6 @@
 (ns ngontinh.ngontinh.homepage
 	(:require 	[ngontinh.ngontinh.libpath :as lib]))
 
-(import 'java.sql.DriverManager)
-
-(defn getTruyen [query]
-	(vec (let 	[connection (DriverManager/getConnection "jdbc:postgresql://23.239.1.206:5432/ngontinh" 
-															"postgres" 
-															"fall2010")
-				mostRead 	(let [	stmt 	(.createStatement connection)
-								 	rs 		(.executeQuery stmt query)]
-								(vec (resultset-seq rs)))
-				addLink		(for [truyen mostRead]
-								(let 	[name 		(truyen :path)
-										linkanh 	(str lib/hostPath "image/" name)
-										linktruyen 	(str lib/hostPath "doctruyen/" name)] 
-									(assoc truyen :linkanh linkanh :linktruyen linktruyen)))
-				ddd 		(.close connection)]
-			addLink)))
-
 
 (def Homepagedata 
 {	:lib-path lib/lib-path 
