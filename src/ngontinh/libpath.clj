443f30745b29f67ed5c3f0d2c1b7f539bfea4cc5
thao ng <roseskindergarten@gmail.com>
2014-05-30 12:54:17 -0400

    jhygtrf

--- src/ngontinh/libpath.clj
+++ src/ngontinh/libpath.clj
@@ -4,7 +4,7 @@
 (def staticPath "http://li679-206.members.linode.com:8000/files/")
 ;(def hostPath "http://li679-206.members.linode.com/")
 (def hostPath "http://localhost:3000/")
-(def local-file-path "/home/thao/Statics/Truyen")
+(def local-file-path "/home/thao/Statics/")
 
 
 (def genres {	"chientruong" "Chiến Trường" 
